4cf8d95f48eb45b39c61e1d3af768525fcea6a05
juanmanuel102341 <jms24@live.com.ar>
2017-11-20 15:35:51 -0300

    genere objeto credits, expresando de donde saque los assets, 2 pantallas q se puede interaccionar entre ellas

---
+++ ProyectoTrafficUniverse/Assets/NextPage.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class NextPage : MonoBehaviour {
+
+	public GameObject Disable;
+	public GameObject Active;
+
+	public void OnActive(){
+
+		Active.SetActive(true);
+		Disable.SetActive(false);
+		}
+
+}
