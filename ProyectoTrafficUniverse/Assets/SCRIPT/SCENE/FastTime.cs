5fdb27c8ec7969a51d4515c24baa085c95c04112
juanmanuel102341 <jms24@live.com.ar>
2017-11-20 17:55:13 -0300

    genere un timer q te adelanta la velocidad del juego

---
+++ ProyectoTrafficUniverse/Assets/SCRIPT/SCENE/FastTime.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+public class FastTime : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void OnIncrement(){
+		print("incrementando tiempo");
+		if(Time.timeScale!=2.5f)
+		Time.timeScale=2.5f;
+	}
+	public void OnNormal(){
+		print("time normal");
+		if(Time.timeScale!=1.0f)
+		Time.timeScale=1.0f;
+	}
+}
