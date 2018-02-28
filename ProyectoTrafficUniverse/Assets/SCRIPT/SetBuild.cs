4e7ca987134c2d58bdd90757ca150fc22b6992ae
juanmanuel102341 <jms24@live.com.ar>
2018-02-28 09:22:37 -0300

    generacion de build muy basico en menu

---
+++ ProyectoTrafficUniverse/Assets/SCRIPT/SetBuild.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+public class SetBuild : MonoBehaviour {
+
+	private Text myBuidNumber;
+	private float num=0.15f;
+	void Awake () {
+		myBuidNumber=GetComponent<Text>();
+		myBuidNumber.text=num.ToString();
+	}
+	
+
+}
